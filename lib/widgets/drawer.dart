aea2e13116952dc0010ed9d751af2ca87f407d22
abracoder <abrakilvis@gmail.com>
2022-06-26 23:04:56 +0530

    day_9 c0mpleted

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -10,7 +10,7 @@ class MyDrawer extends StatelessWidget {
         'https://scontent.fbwa2-1.fna.fbcdn.net/v/t39.30808-6/254644681_1589529191392106_4090418153351531666_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=vd7nZkLli1YAX8y7U5N&_nc_ht=scontent.fbwa2-1.fna&oh=00_AT8uN9E2Sd5lp648Xi2j4JF6IpTBVVyPReoy_LDt5xcvQA&oe=62BE10F7';
     return Drawer(
       child: Container(
-        color: Colors.deepPurple,
+        color: Colors.indigo,
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
@@ -18,13 +18,19 @@ class MyDrawer extends StatelessWidget {
                 padding: EdgeInsets.zero,
                 child: UserAccountsDrawerHeader(
                   margin: EdgeInsets.zero,
-                  accountName: Text("Suraj Gupta"),
-                  accountEmail: Text("surajkg33@gmail.com"),
+                  accountName: const Text(
+                    "Suraj Gupta",
+                    style: TextStyle(color: Colors.green),
+                  ),
+                  accountEmail: const Text(
+                    "surajkg33@gmail.com",
+                    style: TextStyle(color: Colors.green),
+                  ),
                   currentAccountPicture: CircleAvatar(
                     backgroundImage: NetworkImage(imageUrl),
                   ),
                 )),
-            ListTile(
+            const ListTile(
               leading: Icon(
                 CupertinoIcons.home,
                 color: Colors.white,
@@ -37,7 +43,7 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            ListTile(
+            const ListTile(
               leading: Icon(
                 CupertinoIcons.profile_circled,
                 color: Colors.white,
@@ -50,7 +56,7 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            ListTile(
+            const ListTile(
               leading: Icon(
                 CupertinoIcons.envelope_fill,
                 color: Colors.white,
